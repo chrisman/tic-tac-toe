a0ff3fbb4dfb1a4957d37e37911655ec64a57aee
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-10 10:32:43 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+var streamify = require('gulp-streamify')
+var uglify = require('gulp-uglify')
+
+var sass = require('gulp-sass')
+var normalize = require('node-normalize-scss')
+var cssmin = require('gulp-minify-css')
+
+var jade = require('gulp-jade')
+var image = require('gulp-image')
+var livereload = require('gulp-livereload')
+var del = require('del')
+
+gulp.task('html', function(){
+  return gulp.src('src/views/*jade')
+    .pipe(jade())
+    .pipe(gulp.dest('public'))
+});
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded',
+  includePaths: normalize.includePaths
+}
+gulp.task('styles', function(){
+  return gulp.src('src/sass/main.scss')
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(gulp.dest('public/css'))
+    .pipe(cssmin())
+})
+
+gulp.task('js', function(){
+  return browserify('src/js/main.js')
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest('public/js'))
+})
+
+gulp.task('clean', function(){
+  return del(['public/css', 'public/js'])
+})
+
+gulp.task('images', function(){
+  return gulp.src('src/assets/img/*')
+    .pipe(image())
+    .pipe(gulp.dest('public/img'))
+})
+
+gulp.task('watchman', function(){
+  gulp.watch('src/js/**/*js', ['js'])
+  gulp.watch('src/sass/**/*scss', ['styles'])
+  gulp.watch('src/views/**/*jade', ['html'])
+
+  livereload.listen()
+  gulp.watch(['public/**']).on('change', livereload.changed)
+})
+
+gulp.task('default', ['html', 'styles', 'js', 'watchman'])
---
+++ readme.md
@@ -0,0 +1,21 @@
+# tic-tac-toe
+
+This is a game of tic-tac-toe, except it's not just normal tic-tac-toe. it's _3D TIC TAC TOE_, which means it's bigger in every way.
+
+__Rules__
+
+1. First player to place 4 of their markers in a row wins.
+
+2. Markers can be placed orthogonally or diagonally, on the x, y, or z axis.
+
+__MVP__
+
+1. A playable, local pvp game
+
+2. a playable player vs computer game
+
+__Stretch__
+
+1. three.js display?
+
+2. websockets multiplayer
